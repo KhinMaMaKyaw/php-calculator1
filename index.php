13853dfe931030b483d65a17be11d0ce23f8285a
KhinMaMaKyaw <kmmk26615@gmail.com>
2022-05-17 15:24:49 +0630

    design

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Calculator</title>
+</head>
+<body>
+    <div class="container">
+        <div class="left">
+            <h1>PHP CALCULATOR</h1>
+        </div>
+        <div class="right">
+            <form action="" method="get">
+                <input type="text" name="first" placeholder="ENTER NUMBER"><br>
+                <input type="text" name="second" placeholder="ENTER NUMBER"><br>
+                <select name="" id="">
+                    <option value="add">ADD</option>
+                    <option value="sub">SUB</option>
+                    <option value="mul">MUL</option>
+                    <option value="div">DIV</option>
+                </select><br>
+                <button value="submit">SUBMIT</button>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
