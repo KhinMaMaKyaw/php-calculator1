23965626a5054f372fe101b9db1bae390f513f5f
KhinMaMaKyaw <kmmk26615@gmail.com>
2022-05-19 14:18:29 +0630

    insert php

--- index.php
+++ index.php
@@ -13,18 +13,52 @@
             <h1>PHP CALCULATOR</h1>
         </div>
         <div class="right">
-            <form action="" method="get">
-                <input type="text" name="first" placeholder="ENTER NUMBER"><br>
-                <input type="text" name="second" placeholder="ENTER NUMBER"><br>
-                <select name="" id="">
-                    <option value="add">ADD</option>
-                    <option value="sub">SUB</option>
-                    <option value="mul">MUL</option>
-                    <option value="div">DIV</option>
+            
+            <form action="" method="POST">
+                <input type="text" name="first" placeholder="ENTER NUMBER" required><br>
+                <input type="text" name="second" placeholder="ENTER NUMBER" required><br>
+                <select name="operator" id="">
+                    <option value="add" >ADD</option>
+                    <option value="sub" >SUB</option>
+                    <option value="mul" >MUL</option>
+                    <option value="div" >DIV</option>
                 </select><br>
-                <button value="submit">SUBMIT</button>
+                <input class="button" type="submit" value="SUBMIT" name="submit">
             </form>
+            
         </div>
+        <div class="result">
+        <p>
+        <?php
+            if(isset($_POST['submit'])){
+                $first = $_POST['first'];
+                $second = $_POST['second'];
+                $operator = $_POST['operator'];
+           
+                switch($operator){
+                    case "add":
+                        $result = $first + $second;
+                        echo "The addition of two num is {$result}.";
+                        break;
+                    case "sub":
+                        $result = $first - $second;
+                        echo "The substruction of two num is {$result}.";
+                        break;
+                    case "mul":
+                        $result = $first * $second;
+                    echo "The multiplication of two num is {$result}.";
+                        break;
+                    case "div":
+                        $result = $first / $second;
+                        echo "The division of two num is {$result}.";
+                        break;
+                    default: echo "Invalid operator.";
+                }
+            }
+            ?>
+        </p>
+        </div>
+
     </div>
 </body>
 </html>
